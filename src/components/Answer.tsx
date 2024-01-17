060c00b13b8ad314b314d02daa3a30f622c2139d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-17 17:00:03 +0100

    working animation upd

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -4,7 +4,7 @@ function Answer({working, text} : {working : boolean, text : string}){
     return(
         <article className={working ? 'answer working' : 'answer'}  >
             <h3>Answer :</h3>
-            <span>{text}</span>
+            <span className={working ? 'working' : ''}>{text}</span>
         </article>
     )
 }
