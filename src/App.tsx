3bab83c1093a6e8448aa9bd5ab09ea138fe84b68
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-17 17:33:11 +0100

    autoscroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css'
-import { useRef, useState } from 'react'
+import { MutableRefObject, RefObject, useEffect, useRef, useState } from 'react'
 import DialogBlockFactory from './assets/services/DialogBlockFactory';
 
 function App() {
@@ -19,18 +19,29 @@ function App() {
 
   const [history, setHistory] = useState<IHistory[]>([])
 
+  const historyRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    historyRef.current?.scrollTo({
+      top: historyRef.current.scrollHeight,
+      behavior: 'smooth'
+    })
+  }, [historyRef.current?.scrollHeight])
+
   async function handleClick(){
 
+    // href the element?
     const inputValue = (document.getElementById('userMessage') as HTMLTextAreaElement).value
     if(inputValue == null) return
     
     initStreamedDatas()
-    // setHistory(history => history + '\n\nQuestion :\n' + inputValue + '\n\nAnswer :\n')
+    
     setHistory(history => {
       const newHistory = [...history]
       newHistory.push({type : "question", text : inputValue})
       return newHistory
     })
+
     const response = await fetch('http://localhost:3000/chat',
     {
         method: 'POST',
@@ -39,14 +50,19 @@ function App() {
         },
         body: JSON.stringify({"question" : inputValue})      
     })
+
     const reader = response.body?.getReader()
-    // eslint-disable-next-line no-constant-condition
+    
     setHistory(history => {
       const historyDuplicate = [...history]
+      // create a new answer dialog block with a at work animation
       historyDuplicate.push({type : "answer", text : "", working : true})
       return historyDuplicate
     })
+
+    // start reading the stream of datas
     while (true && reader) {
+
       const { done, value } = await reader.read();
       const chunk = decoder.decode(value, { stream: true })
       if(streamedDatasRef.current == "" && chunk.trim() == "") continue
@@ -56,29 +72,26 @@ function App() {
         historyDuplicate[history.length-1].text = streamedDatasRef.current
         return historyDuplicate
       })
+
+      // end of read
       if (done) {
+        // stop the answer block at work animation
         setHistory(history =>
           {
             const duplicateHistory = [...history]
             duplicateHistory[duplicateHistory.length-1].working = false
             return duplicateHistory
           })
-        // Do something with last chunk of data then exit reader
-        // setHistory(history => history  + 'Answer : \n' + streamedDatasRef.current.trim() + '\n\n')
-        /*setHistory(history => {
-          const newHistory = [...history]
-          newHistory.push({type : "answer", text : streamedDatasRef.current})
-          return newHistory
-        })*/
         return
       }
+
     }
   }
 
 
   return (
     <div style={{display:'flex', flexDirection:'column', rowGap:"1rem", minWidth:800}}>
-      <div id="historyContainer">
+      <div ref={historyRef} id="historyContainer">
           {history.map(chunk => DialogBlockFactory(chunk))}
       </div>
       <textarea name="userMessage" id="userMessage"/>
